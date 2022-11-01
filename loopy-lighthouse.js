3fc1b5a47725bc6081d64766aae42298c244ef29
Ryan De Mesa <ryandemesa09@gmail.com>
2022-11-01 01:19:40 +0000

    Loopy Lighthouse example problem

---
+++ loopy-lighthouse.js
@@ -0,0 +1,16 @@
+let loopy = 100;
+
+for (loopy = 100; loopy <= 200; ++loopy) {
+  
+  if (loopy % 3 === 0 && loopy % 4 === 0) {
+    console.log("LoopyLighthouse");
+    
+} else if (loopy % 3 === 0) {
+    console.log("Loopy")
+
+} else if (loopy % 4 === 0) {
+    console.log("Lighthouse");
+
+} else {
+  console.log(loopy);}
+}
